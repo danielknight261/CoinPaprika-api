b242eea73489d164ef57f718c453348a86132efe
danielknight261 <100095189+danielknight261@users.noreply.github.com>
2023-04-17 09:37:05 +0100

    Intial Commit -m

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-6xl font-bold underlin text-red-400">
+      Hello world!
+    </h1>
+    </div>
+  );
+}
+
+export default App;
